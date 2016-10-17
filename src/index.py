c2086cd3879a6466f07a43b62b2087b86faf9bca
Jacint Virag <viragjacint@gmail.com>
2016-10-17 16:04:57 +0000

    Created slider for favourites. Finalised Track page. Added add "Favourite" or remove "favorite" function which updates the database. Created a simple login form.

--- src/index.py
+++ src/index.py
@@ -14,11 +14,40 @@ def root():
 
 @app.route('/favourites')
 def favourites():
-  return render_template('favourites.html'), 200
-  
-@app.route('/track')
+	sql = ('SELECT * FROM mixes WHERE favourite = 1')
+	connection = sqlite3.connect(db_location)
+	connection.row_factory = sqlite3.Row     		
+	rows = connection.cursor().execute(sql).fetchall()	
+	return render_template('favourites.html', rows = rows)
+
+@app.route('/removefav', methods=['GET'])	
+def removefav():
+	get_id = request.args.get('id')	
+	sql = ('UPDATE mixes SET favourite = 0 WHERE id = ?')
+	connection = sqlite3.connect(db_location)
+	connection.row_factory = sqlite3.Row     		
+	connection.cursor().execute(sql, get_id)
+	connection.commit()
+	return redirect(url_for('track', id=get_id))
+
+@app.route('/addfav', methods=['GET'])	
+def addfav():
+	get_id = request.args.get('id')	
+	sql = ('UPDATE mixes SET favourite = 1 WHERE id = ?')
+	connection = sqlite3.connect(db_location)
+	connection.row_factory = sqlite3.Row     		
+	connection.cursor().execute(sql, get_id)
+	connection.commit()
+	return redirect(url_for('track', id=get_id))
+	
+@app.route('/track', methods=['GET'])
 def track():
-  return render_template('track.html'), 200
+	get_id = request.args.get('id')
+	sql = ('SELECT * FROM mixes WHERE id = ?')
+	connection = sqlite3.connect(db_location)
+	connection.row_factory = sqlite3.Row     		
+	rows = connection.cursor().execute(sql, get_id).fetchall()	
+	return render_template('track.html', rows = rows)
 
 @app.route('/genre')
 @app.route('/genre/<type>')
@@ -29,6 +58,16 @@ def genre(type):
 	rows = connection.cursor().execute(sql, [type]).fetchall()
 	return render_template('genre.html', rows = rows, type=type)
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		
+		username = request.form['username']
+		password = request.form['password']
+		return username, password
 
+	else:
+		return render_template('login.html')
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
