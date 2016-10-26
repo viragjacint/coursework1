23a9d6f408d6e57e606da82f8874d72095b53455
Jacint Virag <viragjacint@gmail.com>
2016-10-26 11:25:02 +0000

    Changed db schema, changed text fields to varchar to avid any charchter issues. Chanaged these fields in the db as well

--- src/db.py
+++ src/db.py
@@ -7,7 +7,7 @@ def delete_table():
   c.execute('DROP TABLE mixes;')
 
 def create_table():
-  c.execute('CREATE TABLE IF NOT EXISTS mixes(ID INTEGER PRIMARY KEY AUTOINCREMENT, artist TEXT, favourite INT, lenght VARCHAR(255),genre TEXT, rel_date DATE, alb_img VARCHAR(255), mix_name VARCHAR(255), mp3_name VARCHAR(255), desc TEXT);')
+  c.execute('CREATE TABLE IF NOT EXISTS mixes(ID INTEGER PRIMARY KEY AUTOINCREMENT, artist VARCHAR(255), favourite INT, lenght VARCHAR(255),genre VARCHAR(255), rel_date VARCHAR(255), alb_img VARCHAR(255), mix_name VARCHAR(255), mp3_name VARCHAR(255), desc VARCHAR(255));')
 
 def data_entry():
   c.execute('INSERT INTO mixes (artist, favourite, lenght, genre, rel_date, alb_img, mix_name, mp3_name, desc)  VALUES("Ace Ventura","1", "3:29", "psy", "2014.06.17", "ff.png", "Million Little Peaces", "million.mp3", "A sample of my remix for Million little pieces, bundled with a killer Outsiders remix on HOMmega productions.");')
